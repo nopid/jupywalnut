bfac2b1da93c0790391f148ffdd5f2d49eccb075
Nicolas Ollinger <nicolas.ollinger@univ-orleans.fr>
2023-03-12 21:33:31 +0100

    Better show

--- walnutkernel.py
+++ walnutkernel.py
@@ -5,17 +5,19 @@ from IPython.display import HTML
 from pexpect import spawn
 import os
 
+HOME=os.environ['HOME']
+
 
 class MyMagic(Magic):
     def line_showme(self, lapin):
         """
-        %%showme LAPIN - display Result named lapin
+        %%showme LAPIN - display Result named lapin.gv
         """
         try:
             import pydot
         except:
             raise Exception("You need to install pydot")
-        graph = pydot.graph_from_dot_file("Result/" + str(lapin) + ".gv")[0]
+        graph = pydot.graph_from_dot_file(f"{HOME}/Result/{str(lapin)}.gv")[0]
         svg = graph.create_svg()
         if hasattr(svg, "decode"):
             svg = svg.decode("utf-8")
@@ -41,7 +43,7 @@ class WalnutKernel(ProcessMetaKernel):
         self.register_magics(MyMagic)
 
     def makeWrapper(self):
-        child = spawn("java -jar /data/walnut.jar", cwd=os.environ['HOME'], echo=True, encoding="utf-8")
+        child = spawn("java -jar /data/walnut.jar", cwd=HOME, echo=True, encoding="utf-8")
         child.expect(r"\n")
         return REPLWrapper(child, r"\[Walnut\]\$ ", None, echo=True)
 
